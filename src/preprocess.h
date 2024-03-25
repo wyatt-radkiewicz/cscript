0d8204a6d51360a3dbc6faff1f419e358b9a545b
eklipsed <wyattwradkiewicz@gmail.com>
2024-03-25 12:49:06 -0600

    Making everything NEW'

--- src/preprocess.h
+++
@@ -1,9 +0,0 @@
-#ifndef _preprocess_h_
-#define _preprocess_h_
-
-#include "common.h"
-
-size_t preprocess(char *src, char *buf, size_t buflen);
-
-#endif
-
--- test.c
+++ test.c
@@ -1,5 +1,3 @@
-extern void conlog_s1d(const char *s, int x1);
-
-int main(void) {
-	return 4;
+int main(int argc, char **argv) {
+	return 0;
 }
